41e76b82f40f517ce2f28975b57e14cf3da5e461
baclan228 <dimaisasha52@gmail.com>
2019-02-13 18:52:28 +0300

    V 1.9

--- js/onclick.js
+++ js/onclick.js
@@ -1,32 +1,7 @@
-function hide_left() {
-	$('.left').hide();
-	$('.grid-left').show();
+function menu() {
+	$('.pop_window').fadeIn();
 }
 
-function show_left() {
-	$('.left').show();
-	$('.grid-left').hide();
+function close_menu() {
+	$('.pop_window').fadeOut();
 }
-
-function show_left_menu() {
-	$('.mask').show();
-	$('.left').show();
-	$('.grid-left').hide();
-}
-
-function mask() {
-	$('.mask').hide();
-	$('.left').hide();
-	$('.grid-left').hide();
-}
-
-function grid1() {
-	$('.center__img img').css("width", "260px", "height", "173px");
-	$('.box').css("width", "260px");
-}
-
-function grid2() {
-	$('.center__img img').css("width", "200px", "height", "50px");
-	$('.box').css("width", "200px", "height", "50px");
-}
-
