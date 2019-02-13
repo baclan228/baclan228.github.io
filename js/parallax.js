80f26c45f70f13f375a1c8d2fc614fc72bb1aa76
baclan228 <dimaisasha52@gmail.com>
2019-02-13 15:51:15 +0300

    V 1.5

--- js/parallax.js
+++ js/parallax.js
@@ -7,10 +7,36 @@ $(window).scroll(function() {
 	$('.header').css({
 		"transform" : "translate(0%, "+ st +"%)"
 	});
-	/*console.log(st);
-	if(st > 10){
+	/*console.log(st);*/
+	
+
+	if (st > 609){
+		$('.progress_one').css("width", "60%");
+		$('.progress_two').css("width", "80%");
+		$('.progress_three').css("width", "50%");
 		
+		/*var num = 1;
+		setInterval(function lol() {
+    		num++;
+
+    		if(num > 60){
+    			clearInterval(lol);
+    			$('.progress_one').css("width", "60%");
+    		}else{
+    			console.log(num);
+    			$('.progress_one').css("width", ""+num+"%");
+    		}
+		}, 100);*/
+		
+		
+
 	}else{
-		alert('меньше');
-	}*/
+		$('.progress_one').css("width", "0%");
+		$('.progress_two').css("width", "0%");
+		$('.progress_three').css("width", "0%");
+	}
+
+	if(st > 2754){
+		$('.services_box').css("opacity", "1");
+	}
 });
